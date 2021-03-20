44ef30b4cda9efb7004d9c9c31f107e5768f0f0e
Freddie Brewin <freddiebrewin@live.com>
2021-03-20 19:46:03 +0000

    added local storage functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 var dateDisplayEl = $('#date-display');
 var timeBlock = $('.time-block');
 var currentTime = moment().hours();
+var saveBtn = $('.saveBtn');
 
 function displayTime() {
     var rightNow = moment().format('MMM DD, YYYY [at] hh:mm:ss a');
@@ -20,3 +21,22 @@ timeBlock.each(function() {
     $(this).children(".col-sm-10").attr("class","future col-sm 10 description");
   }
 });
+
+saveBtn.on('click', function (event){
+  event.preventDefault();
+  console.log($(this).siblings().eq(1).val());
+   
+  var text = $(this).siblings().eq(1).val();
+  var parent = $(this).parent().attr('id');
+  localStorage.setItem(parent, JSON.stringify(text));
+})
+
+$('#9 textarea').val(localStorage.getItem('9'));
+$('#10 textarea').val(localStorage.getItem('10'));
+$('#11 textarea').val(localStorage.getItem('11'));
+$('#12 textarea').val(localStorage.getItem('12'));
+$('#13 textarea').val(localStorage.getItem('13'));
+$('#14 textarea').val(localStorage.getItem('14'));
+$('#15 textarea').val(localStorage.getItem('15'));
+$('#16 textarea').val(localStorage.getItem('16'));
+$('#17 textarea').val(localStorage.getItem('17'));
