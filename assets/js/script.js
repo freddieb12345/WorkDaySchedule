f24608baccdfbcc9a1094acc7fe563740be59ad6
Freddie Brewin <freddiebrewin@live.com>
2021-03-20 19:17:08 +0000

    color coded timeblocks according to whether they are in the past present or future

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,22 @@
 var dateDisplayEl = $('#date-display');
+var timeBlock = $('.time-block');
+var currentTime = moment().hours();
 
 function displayTime() {
     var rightNow = moment().format('MMM DD, YYYY [at] hh:mm:ss a');
     dateDisplayEl.text(rightNow);
   }
-  setInterval(displayTime, 1000);
+setInterval(displayTime, 1000);
+
+timeBlock.each(function() {
+  var hour = parseInt($(this).attr('id'));
+  console.log(hour);
+
+  if (hour === currentTime) {
+    $(this).children(".col-sm-10").attr("class","present col-sm 10 description");
+  } else if(hour < currentTime) {
+    $(this).children(".col-sm-10").attr("class","past col-sm 10 description");
+  } else {
+    $(this).children(".col-sm-10").attr("class","future col-sm 10 description");
+  }
+});
